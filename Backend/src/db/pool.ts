5e44dbc3f8c3f5af52f90ae4a72df2e9ce5c6028
biswabaral417 <131336755+biswabaral417@users.noreply.github.com>
2025-08-19 12:51:22 +0545

    tls /ssl sql

--- Backend/src/db/pool.ts
+++ Backend/src/db/pool.ts
@@ -9,7 +9,10 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT || '5432', 10),
+  port: parseInt(process.env.DB_PORT || '5432', 10),  ssl: {
+    rejectUnauthorized: false, // important for Render managed Postgres
+  },
+
 });
 
 
