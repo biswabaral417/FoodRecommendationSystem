6b5b95bd08c673307a3e7361113c9d940b255a76
biswabaral417 <131336755+biswabaral417@users.noreply.github.com>
2025-08-19 13:17:58 +0545

    removing the .envs

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -5,16 +5,25 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(express.json());
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://food-recommendation-system-pi.vercel.app'
+];
+
 app.use(cors({
-  origin: [
-    'http://localhost:5173',                // local dev
-    'https://food-recommendation-system-pi.vercel.app/' // deployed frontend
-  ],
-  credentials: true, // allow cookies
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true); // allow requests
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
 }));
 
+
 app.use(cookieParser());
-// app.use(sessionMiddleware)
+// app.use(sessionMiddleware)   
 
 app.use("/api", router);
 app.get("/helloworld", (req, res) => {
