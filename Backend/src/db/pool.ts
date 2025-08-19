efb04c8e8f3ecde2c0ba6654a6f944c38da0b833
biswabaral417 <131336755+biswabaral417@users.noreply.github.com>
2025-08-19 12:08:24 +0545

    fixing render deployment

--- Backend/src/db/pool.ts
+++ Backend/src/db/pool.ts
@@ -3,12 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT || '5432'),
+  port: parseInt(process.env.DB_PORT || '5432', 10),
 });
 
 
@@ -31,5 +32,5 @@ export default async function connectDB() {
   }
 }
 connectDB()
-export  {pool}
+export { pool }
 
